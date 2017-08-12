9fef381403e45870c0c6febc548047883cdf49f9
Misha Orel <mptich@gmail.com>
2017-08-12 00:43:30 -0700

    Added disclaimer

--- tfHelpers/binFileAccess.py
+++ tfHelpers/binFileAccess.py
@@ -1,8 +1,26 @@
 # Classes to write, and randomly read, from a bin file containing training records
+#
+# Copyright (C) 2017  Author: Misha Orel
+#
+# # This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 
 # Unfortunately, if we use Tensorflow Queues mechanism, we have to give up feed_dict interface.
 # This file allows you to load random training entries at real time, while keeping feed_dict.
 # It also allows to do boosting, by assigning different probabilities for loading different entries
+
+
 
 __author__ = "Misha Orel"
 
