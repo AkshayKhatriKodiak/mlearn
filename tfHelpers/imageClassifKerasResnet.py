2decbe6a6873ec4900b6279fdb2cce47d2afc73e
Mike Orel <morel@morel-mac.attlocal.net>
2018-08-07 04:24:30 -0700

    Added features to rsnet wrapper.

--- tfHelpers/imageClassifKerasResnet.py
+++ tfHelpers/imageClassifKerasResnet.py
@@ -42,9 +42,54 @@ TrainingBatchCount = 10
 
 class ImageClassifKerasResnet:
 
+  allowedArchitects_ = [18, 34, 50, 101, 152]
+
+  def __init__(self, classCount=None, imgHeight=None, imgWidth=None,
+    clrMeans=None, clrStds=None, modelName=None, learningRate=0.,
+    architectureNumber=18, batchSize=1):
+
+    assert architectureNumber in ImageClassifKerasResnet.allowedArchitects_
+
+    self.classCount_ = classCount
+    self.height_ = imgHeight
+    self.width_ = imgWidth
+    self.clrMeans_ = clrMeans
+    self.clrStds_ = clrStds
+    self.batchSize_ = batchSize
+
+    if modelName is None:
+      # Creating model from scratch
+      assert learningRate is not None
+      architectureName = ('build_resnet_%d' % architectureNumber)
+      architectureFunc = getattr(resnet.ResnetBuilder, architectureName)
+      self.model_ = architectureFunc((ImageChannels, imgHeight, imgWidth),
+        classCount)
+      sgd = optimizers.SGD(lr=learningRate, decay=0., momentum=0.9, nesterov=True)
+      self.model_.compile(loss='categorical_crossentropy',
+                    # optimizer='adam',
+                    optimizer=sgd,
+                    metrics=['accuracy'])
+    else:
+      # Loading model from file
+      self.model_ = keras.models.load_model(modelName)
+      if learningRate is None:
+        try:
+          learningRate = K.get_value(self.model_.optimizer.lr)
+        except:
+          # If learning rate has not been saved as part of a model
+          # (eg inference model only), then leave it as None
+          pass
+
+    self.learningRate_ = learningRate
+    self.learningRateScale_ = None
+
+    self.classCount_ = self.getClassCount()
+    self.height_, self.width_ = self.getInputShape()
+
+
   # Dictionaries map file name to a class id
-  def __init__(self, trainFilesDict, validFilesDict, classCount, imgHeight, imgWidth, clrMeans, clrStds, augmenter,
-               batchSize, bestModelName=None):
+  def trainInit(self, trainFilesDict, validFilesDict, augmenter,
+               bestModelName=None):
     # Calculates weights for each class in teh raining set
     counts = [0] * classCount
     for cl in trainFilesDict.values():
@@ -60,35 +105,9 @@ class ImageClassifKerasResnet:
       for fn in self.trainFilesList_] 
     self.validFilesDict_ = validFilesDict
     self.validFilesList_ = list(self.validFilesDict_.keys())
-    self.classCount_ = classCount
-    self.height_ = imgHeight
-    self.width_ = imgWidth
-    self.clrMeans_ = clrMeans
-    self.clrStds_ = clrStds
     self.augmenter_ = augmenter
-    self.batchSize_ = batchSize
     self.bestModelName_ = bestModelName
     self.bestAccuracy_ = 1.0
-
-  def modelInit(self, modelName=None, learningRate=None, architectureName='build_resnet_18'):
-    if modelName is None:
-      # Creating model from scratch
-      assert learningRate is not None
-      architectureFunc = getattr(resnet.ResnetBuilder, architectureName)
-      self.model_ = architectureFunc((ImageChannels, self.height_, self.width_), self.classCount_)
-      sgd = optimizers.SGD(lr=learningRate, decay=0., momentum=0.9, nesterov=True)
-      self.model_.compile(loss='categorical_crossentropy',
-                    # optimizer='adam',
-                    optimizer=sgd,
-                    metrics=['accuracy'])
-    else:
-      # Loading model from file
-      self.model_ = keras.models.load_model(modelName)
-      if learningRate is None:
-        learningRate = K.get_value(self.model_.optimizer.lr)
-
-    self.learningRate_ = learningRate
-    self.learningRateScale_ = None
 
 
   def modelSave(self, modelName):
@@ -97,6 +116,22 @@ class ImageClassifKerasResnet:
   def summary(self):
     return self.model_.summary()
 
+  def getInputShape(self):
+    shape = self.model_.layers[0].input.get_shape()[1:3]
+    return((shape[0], shape[1]))
+
+  def getClassCount(self):
+    return self.model_.layers[-1].output.get_shape()[1]
+
+  def imgNormalize(self, img):
+    return (img - self.clrMeans_) / self.clrStds_
+
+  def fileToInput(self, imageFileName):
+    img = UtilImageFileToArray(imageFileName)
+    img = UtilImageResize(img, self.height_, self.width_)
+    img = self.imgNormalize(img)
+    return img
+
   def train(self, rounds):
     def _trainCback(state, lock):
       fileName = np.random.choice(self.trainFilesList_,
@@ -104,7 +139,7 @@ class ImageClassifKerasResnet:
       img = UtilImageFileToArray(fileName)
       assert img.shape[:2] == (self.height_, self.width_)
       img = self.augmenter_.augment(img)
-      img = (img - self.clrMeans_) / self.clrStds_
+      img = self.imgNormalize(img)
       return (img, self.trainFilesDict_[fileName])
 
     # Overwrite learning rate
@@ -138,6 +173,7 @@ class ImageClassifKerasResnet:
 
     mthreadTraining.terminate()
 
+
   def validate(self):
     def _valid_cback(state, lock):
       lock.acquire()
@@ -148,7 +184,7 @@ class ImageClassifKerasResnet:
         return None
       fileName = self.validFilesList_[ind]
       img = UtilImageFileToArray(fileName)
-      img = (img - self.clrMeans_) / self.clrStds_
+      img = self.imgNormalize(img)
       return (img, fileName, self.validFilesDict_[fileName])
 
     validationTuples = []
@@ -182,3 +218,72 @@ class ImageClassifKerasResnet:
         print('Model saved in %s' % self.bestModelName_)
     return errorFileList
 
+  def predict(self, imageFileName):
+    img = self.fileToInput(imageFileName)
+    imgArray = np.stack([img], axis=0)
+    predictions = self.model_.predict(imgArray,
+      batch_size=self.batchSize_, verbose=1)
+    return np.argmax(predictions[0])
+
+  def classActivationMap(self, imageFileName, outCamFile):
+    """
+    outCamFile is a name of output image file containing Class
+    Activation Map superimposed on the input image
+    """
+
+    # First do normal prediction
+    classPred = self.predict(imageFileName)
+
+    # Last layer is linear regression -> soft max input values
+    classWeights = self.model_.layers[-1].get_weights()[0]
+    # Final convolution layer in resnet is -4
+    finalConvOutput = self.model_.layers[-4].output
+
+    # Let's get function mapping input to outputs of last conv layer
+    # and softmax
+    func = K.function([self.model_.layers[0].input], [finalConvOutput])
+    img = self.fileToInput(imageFileName)
+    imgArray = np.stack([img], axis=0)
+    lastConvOutput = func([imgArray])[0][0]
+    assert len(lastConvOutput.shape) == 3 # height x width x conv channels
+
+    # Class activation map.
+    clActMap = np.zeros(dtype = np.float32, shape = lastConvOutput.shape[:2])
+    for ind, weight in enumerate(classWeights[:, classPred]):
+      clActMap += weight * lastConvOutput[:, :, ind]
+
+    # TODO - combine with input image, save
+    print('CAM:\n%s' % str(clActMap))
+    
+
+# If used as utility
+
+def _dumpSummary():
+  classCount = int(sys.argv[2])
+  height = int(sys.argv[3])
+  width = int(sys.argv[4])
+  architecture = int(sys.argv[5])
+ 
+  c = ImageClassifKerasResnet(classCount, height, width,
+    architectureNumber=architecture)
+  print(c.summary())
+
+
+def _predict():
+  modelName = sys.argv[2]
+  imageName = sys.argv[3]
+  metaDataFile = sys.argv[4]
+
+  with open(metaDataFile, 'rb') as fin:
+    clrMeans, clrStds = dill.load(fin)
+
+  c = ImageClassifKerasResnet(clrMeans=clrMeans, clrStds=clrStds,
+    modelName=modelName)
+  print('PREDICTION: %d' % c.predict(imageName))
+  
+
+if __name__ == '__main__':
+  if sys.argv[1] == 'summary':
+    _dumpSummary()
+  elif sys.argv[1] == 'predict':
+    _predict()
